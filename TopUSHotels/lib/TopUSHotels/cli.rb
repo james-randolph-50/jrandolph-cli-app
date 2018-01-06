a6532cc2df5ec06d68e997b1f51415da4a7b64dd
James Randolph <james.randolph2016@gmail.com>
2018-01-06 00:21:48 +0000

    Instructs user to select grouping of top 15 hotelsin the start method, get's thier input

--- TopUSHotels/lib/TopUSHotels/cli.rb
+++ TopUSHotels/lib/TopUSHotels/cli.rb
@@ -3,3 +3,9 @@ class TopUSHotels::CLI
   def call
     TopUSHotels::Scraper.new.make_hotels
     puts "Read about the top 15 Hotels in the United States!"
+    start
+  end
+
+  def start
+    puts "Select a group of hotels: 1-5, 6-10, 11-15"
+    input = get_strip.to_i
