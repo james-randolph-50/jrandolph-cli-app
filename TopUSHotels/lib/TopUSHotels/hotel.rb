c47a50357b82c3b8b8fc837f2dd36f53f5e466e6
James Randolph <james.randolph2016@gmail.com>
2018-01-06 01:04:20 +0000

    defined the init method, with appropraite variables. lastly, appending the instance of self into the @@all array.

--- TopUSHotels/lib/TopUSHotels/hotel.rb
+++ TopUSHotels/lib/TopUSHotels/hotel.rb
@@ -2,4 +2,12 @@ class TopUSHotels::Hotel
   attr_accessor :name, :rank, :location, :description
 
   @@all = []
+
+  def initialize(name=nil, rank=nil, location=nil, description=nil)
+    @name = name
+    @rank = rank
+    @location = location
+    @position = position
+    @@all << self
+  end
 end
