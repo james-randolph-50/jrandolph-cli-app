b1e15304946f51b776604875339081bb6ff2ada3
James Randolph <james.randolph2016@gmail.com>
2018-01-12 19:11:07 +0000

    ID'd last two elements

--- TopUSHotels/lib/TopUSHotels/scraper.rb
+++ TopUSHotels/lib/TopUSHotels/scraper.rb
@@ -8,6 +8,8 @@ def scrape
   doc.search(".search-content").children.css("div").each { |h|
 	   h.name = h.search("a-ellipsis-text search-results-link").text
 	   h.location = h.search(".block-tight text-small").text
+     h.rank = h.search("block-tight search-result-ranking-heading strong").text
+     h.description = h.search("description text-small pad-right-normal-for-small-only ellipsis-block-custom").text
    }
 end
 
